6c2425eeee53e71c77c60fd9ad16af2507b90b63
Radek <radekwawrzyk@gmail.com>
2020-11-20 19:07:21 +0100

    FloatingIcon component

--- components/main/MainHeader/MainHeader.vue
+++ components/main/MainHeader/MainHeader.vue
@@ -12,12 +12,61 @@
       </div>
     </div>
 
+    <floating-icon
+      left="15%"
+      top="20%"
+      icon="polygon"
+      hide-on-mobiles
+    />
+
+    <floating-icon
+      left="40%"
+      top="20%"
+      icon="grid"
+    />
+
+    <floating-icon 
+      icon="plus"
+      top="70%"
+      left="5%"
+      fill="#EE664E"
+      stroke="#EE664E"
+    />
+
+    <floating-icon 
+      icon="ball"
+      top="70%"
+      left="45%"
+      fill="#EE664E"
+      stroke="#EE664E"
+    />
+
+    <floating-icon 
+      icon="grid"
+      top="20%"
+      left="85%"
+    />
+
+    <floating-icon 
+      icon="polygon"
+      top="60%"
+      left="80%"
+    />
+
+    <floating-icon 
+      icon="music"
+      top="40%"
+      left="60%"
+    />
+
     <scroll-down />
   </header>
 </template>
 
 <script>
+import FloatingIcon from '~/components/reusable/FloatingIcon/FloatingIcon.vue';
 export default {
+  components: { FloatingIcon },
   name: 'MainHeader',
   props: {
     heading: {
--- components/reusable/FloatingIcon/FloatingIcon.vue
+++ components/reusable/FloatingIcon/FloatingIcon.vue
@@ -1,16 +1,86 @@
 <template>
-  <div class="floating-icon">
-    xd
+  <div 
+    class="floating-icon"
+    :class="[
+      hideOnMobiles ? 'floating-icon--hide-on-mobile' : false,
+    ]"
+    :style="{
+      top: top,
+      left: left,
+      animationName: `floating-${randomAnimationType}`,
+      animationDelay: `0.${randomAnimationType * 2}s`,
+      width: size,
+      height: size,
+    }"
+  >
+    <ball-icon 
+      v-if="icon === 'ball'"
+      class="floating-icon__inner"
+      :fill="fill"
+      :stroke="stroke"
+    />
+    <music-icon 
+      v-else-if="icon === 'music'"
+      class="floating-icon__inner"
+      :fill="fill"
+      :stroke="stroke"
+    />
+    <grid-icon 
+      v-else-if="icon === 'grid'"
+      class="floating-icon__inner"
+      :fill="fill"
+      :stroke="stroke"
+    />
+    <pause-icon 
+      v-else-if="icon === 'pause'"
+      class="floating-icon__inner"
+      :fill="fill"
+      :stroke="stroke"
+    />
+    <plus-icon 
+      v-else-if="icon === 'plus'"
+      class="floating-icon__inner"
+      :fill="fill"
+      :stroke="stroke"
+    />
+    <polygon-icon 
+      v-else
+      class="floating-icon__inner"
+      :fill="fill"
+      :stroke="stroke"
+      :width="size"
+      :height="size"
+    />
   </div>
 </template>
 
 <script>
+import BallIcon from '~/assets/icons/ball.svg';
+import MusicIcon from '~/assets/icons/music.svg';
+import GridIcon from '~/assets/icons/grid.svg';
+import PauseIcon from '~/assets/icons/pause.svg';
+import PlusIcon from '~/assets/icons/plus.svg';
+import PolygonIcon from '~/assets/icons/polygon.svg';
+import TestIcon from '~/assets/icons/test.svg';
+
+import { getRandom } from '~/helpers/index.js';
+
 export default {
   name: 'FloatingIcon',
+  components: {
+    BallIcon,
+    MusicIcon,
+    GridIcon,
+    PauseIcon,
+    PlusIcon,
+    PauseIcon,
+    PolygonIcon,
+    TestIcon,
+  },
   props: {
     icon: {
       type: String,
-      required: false,
+      required: true,
       default: 'ball',
     },
     fill: {
@@ -22,6 +92,31 @@ export default {
       type: String,
       required: false,
       default: '#ffffff',
+    },
+    left: {
+      type: [Number, String],
+      required: false,
+      default: 0,
+    },
+    top: {
+      type: [Number, String],
+      required: false,
+      default: 0,
+    },
+    size: {
+      type: [Number, String],
+      required: false,
+      default: '64px'
+    },
+    hideOnMobiles: {
+      type: Boolean,
+      required: false,
+      default: false,
+    }
+  },
+  computed: {
+    randomAnimationType() {
+      return getRandom(1, 6);
     },
   },
 };
